3b6b1188a619ded548a7c6fb568f8f93c092b60d
TivaR8 <tiva.rait@ocsbstudent.ca>
2018-05-10 14:28:22 -0400

    Initial commit

---
+++ PercentageProgramTivaR/PercentageProgramForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace PercentageProgramTivaR
+{
+    partial class frmPercentageProgram
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.txtUserLevel = new System.Windows.Forms.TextBox();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Location = new System.Drawing.Point(41, 27);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(99, 13);
+            this.lblInstructions.TabIndex = 0;
+            this.lblInstructions.Text = "Enter a grade level:";
+            // 
+            // txtUserLevel
+            // 
+            this.txtUserLevel.Location = new System.Drawing.Point(182, 27);
+            this.txtUserLevel.Name = "txtUserLevel";
+            this.txtUserLevel.Size = new System.Drawing.Size(100, 20);
+            this.txtUserLevel.TabIndex = 1;
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(138, 102);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(75, 23);
+            this.btnEnter.TabIndex = 2;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // frmPercentageProgram
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 161);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.txtUserLevel);
+            this.Controls.Add(this.lblInstructions);
+            this.Name = "frmPercentageProgram";
+            this.Text = "Percentage Program By Tiva Rait";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.TextBox txtUserLevel;
+        private System.Windows.Forms.Button btnEnter;
+    }
+}
+
---
+++ PercentageProgramTivaR/PercentageProgramForm.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+ * Created by: Tiva Rait
+ * Created on: 10-05-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #30 - Percentage Program
+ * This program...
+*/
+
+namespace PercentageProgramTivaR
+{
+    public partial class frmPercentageProgram : Form
+    {
+        public frmPercentageProgram()
+        {
+            InitializeComponent();
+        }
+
+        // Function: ConvertToPercent
+        // Input: String level
+        // Output: int percentage
+        // Description: This function accepts a level of type string and returns the percentage equivalent as an integer.
+        private int ConvertToPercent(String level)
+        {
+            int percentage;
+            switch (level)
+            {
+                case "4++":
+                    percentage = 98;
+                    break;
+                case "4+":
+                    percentage = 95;
+                    break;
+                case "4-":
+                    percentage = 87;
+                    break;
+                case "3+":
+                    percentage = 78;
+                    break;
+                case "3":
+                    percentage = 75;
+                    break;
+                case "3-":
+                    percentage = 72;
+                    break;
+                case "2+":
+                    percentage = 68;
+                    break;
+                case "2":
+                    percentage = 65;
+                    break;
+                case "2-":
+                    percentage = 62;
+                    break;
+                case "1+":
+                    percentage = 58;
+                    break;
+                case "1":
+                    percentage = 55;
+                    break;
+                case "1-":
+                    percentage = 52;
+                    break;
+                case "0+":
+                    percentage = 45;
+                    break;
+                case "0":
+                    percentage = 40;
+                    break;
+                case "0-":
+                    percentage = 35;
+                    break;
+                case "0--":
+                    percentage = 30;
+                    break;
+                case "0---":
+                    percentage = 20;
+                    break;
+                case "0----":
+                    percentage = 10;
+                    break;
+                default:
+                    percentage = -1;
+                    break;
+            }
+
+            return percentage;
+        }
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            // Declare variables 
+            string level;
+            int mark;
+
+            // Assign variable
+            level = (txtUserLevel.Text);
+
+            // Call the function
+            mark = ConvertToPercent(level);
+
+            // Show the message box
+            MessageBox.Show("Level " + level + " is equivalent to " + mark + "%", "Percentage Program");
+        }
+    }
+}
